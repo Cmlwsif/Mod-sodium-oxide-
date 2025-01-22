b5981245b0aefc6daf621dbd2cefbab232a9dd42
Cmlwsif_ <kirka5625@gmail.com>
2025-01-22 20:33:31 +0500

    Add files via upload

---
+++ src/main/java/com/example/myoptimizationmod/MyOptimizationModClient.java
@@ -0,0 +1,10 @@
+package com.example.myoptimizationmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MyOptimizationModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+        System.out.println("Sodium Oxide Mod initialized!");
+    }
+}
